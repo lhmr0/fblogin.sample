dafaa830f17c176b81c75e1fdba95158ae1cffc7
luishmr0 <luchiitoz@gmail.com>
2017-05-16 00:43:58 -0500

    update

---
+++ app/src/main/java/com/example/c3p0/fbsample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.c3p0.fbsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
